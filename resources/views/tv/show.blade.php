8af69ed4b678816b7f989d5591f4150cf41a5699
Julian-Makholy <julian5m597@gmail.com>
2022-09-14 18:59:15 +0300

    Version1

--- resources/views/tv/show.blade.php
+++ resources/views/tv/show.blade.php
@@ -37,7 +37,7 @@
                     @if (count($tvshow['videos']['results']) > 0)
                         <div class="mt-12">
                             <button
-                                @click="isOpen = true"
+                            wire:click="isOpen = true"
                                 class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
                             >
                                 <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
@@ -54,8 +54,8 @@
                                     <div class="bg-gray-900 rounded">
                                         <div class="flex justify-end pr-4 pt-2">
                                             <button
-                                                @click="isOpen = false"
-                                                @keydown.escape.window="isOpen = false"
+                                            wire:click="isOpen = false"
+                                            wire:keydown.escape.window="isOpen = false"
                                                 class="text-3xl leading-none hover:text-gray-300">&times;
                                             </button>
                                         </div>
@@ -105,7 +105,7 @@
                 @foreach ($tvshow['images'] as $image)
                     <div class="mt-8">
                         <a
-                            @click.prevent="
+                            wire:click.prevent="
                                 isOpen = true
                                 image='{{ 'https://image.tmdb.org/t/p/original/'.$image['file_path'] }}'
                             "
@@ -126,8 +126,8 @@
                     <div class="bg-gray-900 rounded">
                         <div class="flex justify-end pr-4 pt-2">
                             <button
-                                @click="isOpen = false"
-                                @keydown.escape.window="isOpen = false"
+                            wire:click="isOpen = false"
+                            wire:keydown.escape.window="isOpen = false"
                                 class="text-3xl leading-none hover:text-gray-300">&times;
                             </button>
                         </div>
